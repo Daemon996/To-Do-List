7e860e767d5fe80abac7d160362cd176e2413f62
Nathan Courtney <nathan@courtney.org.za>
2025-02-25 21:37:50 +0200

    Update for CF Pages

---
+++ build/precache-manifest.333c34330c0738ec46a350efb11cd44b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07b3de604fe00fd626e0bf00f681910f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e98e39c6f8fe46bf02b",
+    "url": "/static/css/main.000c1ad5.chunk.css"
+  },
+  {
+    "revision": "fed0b2349618354156ec",
+    "url": "/static/js/2.9c8f598b.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/static/js/2.9c8f598b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3332bdd9cfd426e19d0",
+    "url": "/static/js/3.61935d8c.chunk.js"
+  },
+  {
+    "revision": "5e98e39c6f8fe46bf02b",
+    "url": "/static/js/main.8cbe84e8.chunk.js"
+  },
+  {
+    "revision": "108e2bd7d09c048f09ce",
+    "url": "/static/js/runtime-main.6a50ad82.js"
+  }
+]);
